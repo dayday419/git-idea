315bd208663cf015fc74b179221cdb029bd662be
dayday419 <gyt915104@163.com>
2023-03-13 17:00:00 +0800

    first git-idea

---
+++ src/com/dayday/git/MyFirstGit.java
@@ -0,0 +1,14 @@
+package com.dayday.git;
+
+/**
+ * @ClassName: MyFirstGit
+ * @Package: com.dayday.git
+ * @Date: 2023/3/13 16:57
+ * @Author: dayday419
+ * @Description:
+ */
+public class MyFirstGit {
+    public static void main(String[] args) {
+        System.out.println("这是我的第一个git-idea");
+    }
+}
