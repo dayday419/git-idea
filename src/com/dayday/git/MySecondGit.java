2fa2f81fc9de3a0cc980efb9a04e94562a5ce974
dayday419 <gyt915104@163.com>
2023-03-13 17:19:28 +0800

    master 2

---
+++ src/com/dayday/git/MySecondGit.java
@@ -0,0 +1,14 @@
+package com.dayday.git;
+
+/**
+ * @ClassName: MySecondGit
+ * @Package: com.dayday.git
+ * @Date: 2023/3/13 17:18
+ * @Author: dayday419
+ * @Description:
+ */
+public class MySecondGit {
+    public static void main(String[] args) {
+        System.out.println("这是第二个");
+    }
+}
